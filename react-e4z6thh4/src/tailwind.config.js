f447a7bca52e6c15c20bb4695a4e57e8ceaddff2
wilson7878 <2315265782@qq.com>
2025-06-14 18:33:15 +0800

    Create tailwind.config.js

---
+++ react-e4z6thh4/src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 让 Tailwind 识别你所有组件里的 className
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
